dcb987eff1c2cc1995fc94d73512eeafbcf0c04c
Szymon Hudziak <szymonh.dg@gmail.com>
2024-03-20 22:45:46 +0100

    Add task getting data from bluetooth module

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <SoftwareSerial.h>
 
 #define trigPin 4
 #define echoPin 5
 
 void TaskGetObstacleDistance(void *pvParameters);
+void TaskGetBluetoothCommand(void *pvParameters);
 
 int getDistance();
 
 volatile byte hcsr04_sampling = 20;
 volatile int obstacleDistance;
 
+String command = "";
+
+SoftwareSerial BTSerial(2, 3); // RX, TX
+
 void setup()
 {
   Serial.begin(9600);
+  BTSerial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
   xTaskCreate(
       TaskGetObstacleDistance, "GetObstacleDistance", 128, NULL, 2, NULL);
+
+  xTaskCreate(
+      TaskGetBluetoothCommand, "GetBluetoothCommand", 128, NULL, 2, NULL);
 }
 
 void loop()
@@ -32,9 +42,23 @@ void TaskGetObstacleDistance(void *pvParameters)
   for (;;)
   {
     obstacleDistance = getDistance();
-    Serial.print(obstacleDistance); //debug only
-    Serial.println(" cm"); //debug only
+    // Serial.print(obstacleDistance); //debug only
+    // Serial.println(" cm"); //debug only
     vTaskDelay(((1 / hcsr04_sampling) * 1000) / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskGetBluetoothCommand(void *pvParameters)
+{
+  (void)pvParameters;
+
+  for (;;)
+  {
+    if (BTSerial.available())
+    {
+      command = BTSerial.readStringUntil('\n');
+      Serial.println(command); //debug only
+    }
   }
 }
 
