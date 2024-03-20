b1da3aee21783a476d5bb48aa6a9c6e0e882f588
Szymon Hudziak <szymonh.dg@gmail.com>
2024-03-20 22:06:14 +0100

    Add task measuring distance to an obstacle

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define trigPin 4
+#define echoPin 5
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void TaskGetObstacleDistance(void *pvParameters);
+
+int getDistance();
+
+volatile byte hcsr04_sampling = 20;
+volatile int obstacleDistance;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  xTaskCreate(
+      TaskGetObstacleDistance, "GetObstacleDistance", 128, NULL, 2, NULL);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void TaskGetObstacleDistance(void *pvParameters)
+{
+  (void)pvParameters;
+
+  for (;;)
+  {
+    obstacleDistance = getDistance();
+    Serial.print(obstacleDistance); //debug only
+    Serial.println(" cm"); //debug only
+    vTaskDelay(((1 / hcsr04_sampling) * 1000) / portTICK_PERIOD_MS);
+  }
+}
+
+int getDistance()
+{
+  long time, distance;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  time = pulseIn(echoPin, HIGH);
+  distance = time / 58;
+
+  return distance;
 }
