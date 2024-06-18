def37e2a055c3f6302ed59b4c0eee84f9b2af73a
Szymon Hudziak <szymonh.dg@gmail.com>
2024-06-18 17:57:08 +0200

    Add functions to control motors

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,57 @@
 #include <SoftwareSerial.h>
 #include "DHT.h"
 
-#define trigPin 4
-#define echoPin 5
-#define DHT11_PIN 6
+// Distance sensor
+#define trigPin A4
+#define echoPin A3
 
+// Humidity sensor
+#define DHT11_PIN A5
+
+// Right motor
+#define M1PWM 9
+#define M1DIR 7
+#define rightSpeed 75
+
+// Left Motor
+#define M2PWM 10
+#define M2DIR 8
+#define leftSpeed 75
+
+// Tasks for robot's modules
 void TaskHCSR4(void *pvParameters);
 void TaskXM15(void *pvParameters);
 void TaskDHT11(void *pvParameters);
+void TaskMotors(void *pvParameters);
 
+// Objects for semaphore, bluetooth module and dht11 sensor
 SemaphoreHandle_t xSemaphore;
 SoftwareSerial BTSerial(2, 3); // RX, TX
 DHT dht;
 
+// Functions to control directions of motors
+void goForward();
+void goBackward();
+void turnRight();
+void turnLeft();
+void stopMotors();
+
+// Function getting distance from HC-SR04 sensor
 int getDistance();
 
-volatile byte hcsr04_sampling = 20; // frequency of detection in Hz
-volatile int obstacleDistance;
-volatile int temperature;
-volatile int humidity;
+// Variables for sensors
+volatile byte hcsr04_sampling = 10; // frequency of detection in Hz
+volatile int obstacleDistance = 0;
+volatile int temperature = 0;
+volatile int humidity = 0;
 volatile int minPeriod = dht.getMinimumSamplingPeriod();
 
 String command = "";
+String previousCommand = "";
 
 void setup()
 {
+  // Initialize communication and configure pins
   Serial.begin(9600);
   BTSerial.begin(9600);
   pinMode(trigPin, OUTPUT);
@@ -64,8 +91,8 @@ void TaskHCSR4(void *pvParameters)
     if (xSemaphoreTake(xSemaphore, (TickType_t)5) == pdTRUE)
     {
       obstacleDistance = getDistance();
-      // Serial.print(obstacleDistance); //debug only
-      // Serial.println(" cm"); //debug only
+      Serial.print(obstacleDistance); //debug only
+      Serial.println(" cm"); //debug only
       xSemaphoreGive(xSemaphore);
       vTaskDelay(((1 / hcsr04_sampling) * 1000) / portTICK_PERIOD_MS);
     }
@@ -83,7 +110,7 @@ void TaskXM15(void *pvParameters)
       if (BTSerial.available())
       {
         command = BTSerial.readStringUntil('\n');
-        // Serial.println(command); // debug only
+        Serial.println(command); // debug only
       }
       xSemaphoreGive(xSemaphore);
     }
@@ -112,6 +139,49 @@ void TaskDHT11(void *pvParameters)
       vTaskDelay(minPeriod / portTICK_PERIOD_MS);
     }
   }
+}
+
+void TaskMotors(void *pvParameters)
+{
+
+}
+
+void goForward()
+{
+  digitalWrite(M1DIR, HIGH);
+  digitalWrite(M2DIR, HIGH);
+  analogWrite(M1PWM, rightSpeed);
+  analogWrite(M2PWM, leftSpeed);
+}
+
+void goBackward()
+{
+  digitalWrite(M1DIR, LOW);
+  digitalWrite(M2DIR, LOW);
+  analogWrite(M1PWM, rightSpeed);
+  analogWrite(M2PWM, leftSpeed);
+}
+
+void turnRight()
+{
+  digitalWrite(M1DIR, LOW);
+  digitalWrite(M2DIR, HIGH);
+  analogWrite(M1PWM, rightSpeed);
+  analogWrite(M2PWM, leftSpeed);
+}
+
+void turnLeft()
+{
+  digitalWrite(M1DIR, HIGH);
+  digitalWrite(M2DIR, LOW);
+  analogWrite(M1PWM, rightSpeed);
+  analogWrite(M2PWM, leftSpeed);
+}
+
+void stopMotors()
+{
+  analogWrite(M1PWM, 0);
+  analogWrite(M2PWM, 0);
 }
 
 int getDistance()
